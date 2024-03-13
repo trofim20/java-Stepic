fea1c9a6bb43da8f4bd81a5f131eeb4c2a56ed23
trofim20 <127005753+trofim20@users.noreply.github.com>
2024-03-13 19:01:40 +0500

    Add files via upload

---
+++ L2.1 Authorization/src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignInServlet extends HttpServlet {
+
+    private final AccountService accountService;
+
+    public SignInServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if (login == null || password == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile == null || !password.equals(profile.getPass())) {
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Unauthorized");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        } else {
+
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Authorized: " + login);
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
+
+}
